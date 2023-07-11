ebd7e8aa89137376fdb4e86bd32a347396961df6
pramodtng <psingay96@gmail.com>
2023-07-11 10:31:25 +0600

    products-updated

--- app/component/home/BannerSection.jsx
+++ app/component/home/BannerSection.jsx
@@ -13,15 +13,15 @@ class BannerSection extends Component {
     };
 
     return (
-      <section className="py-28  min-h-screen relative before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-no-repeat before:bg-contain before:z-0">
+        <section className="py-28  min-h-screen relative before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-no-repeat before:bg-contain before:z-0 ">
         <div className="container max-w-6xl mx-auto relative z-10 px-2">
-          <div className="grid lg:grid-cols-[1fr_1fr] sm:grid-cols-1 items-center gap-5 min-h-[calc(100vh-160px)]">
+          <div className="grid lg:grid-cols-[1fr_1fr] sm:grid-cols-1 items-center gap-5 min-h-[calc(100vh-160px)] ">
             <div className="pt-2">
-              <h2 className="text-blue font-bold font-poppins lg:text-[52px]  text-4xl lg:leading-[60px] leading-10 lg:mb-8 mb-6 lg:text-left md:text-center text-center">
+              <h2 className="text-white font-bold font-poppins lg:text-[52px]  text-4xl lg:leading-[60px] leading-10 lg:mb-8 mb-6 lg:text-left md:text-center text-center">
                 Software <br /> Development <br /> Unit
               </h2>
               <p className="lg:text-xl text-base leading-7 mb-8 lg:text-left md:text-center text-center">
-                We are a Software Development Unit based IT company in Thimphu
+                We are a Software Development Unit based IT compassny in Thimphu
                 who provides top quality service.
               </p>
               <p className="lg:text-left md:text-center text-center">
