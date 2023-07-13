5637468165f6627cada80798341da2881196ff3b
pramodtng <psingay96@gmail.com>
2023-07-13 11:00:12 +0600

    eslint-errors

--- app/component/product/ProductsShow.jsx
+++ app/component/product/ProductsShow.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-children-prop */
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import React, { useState } from "react";
--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import { FaPhoneAlt, FaSearchLocation, FaEnvelopeOpen } from "react-icons/fa";
 
